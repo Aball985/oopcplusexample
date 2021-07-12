15be5be648b1c7cbfb37f3724c7d6bb9e9ac1a30
unknown <arball44@gmail.com>
2021-07-11 23:14:53 -0400

    Finished OOP

--- oop.cpp
+++ oop.cpp
@@ -8,20 +8,23 @@ class IEmployee
     virtual void askForPromotion() = 0;
 };
 
-// Class Employee interface
-// Contract Interface Employee
+//Class Employee interface
+//Contract Interface Employee
 class Employee : IEmployee
 {
-    // Private variables
+    //Private variables
 private:
-    // Encapsulation example as these are only accesed through get and set methods
-    string employeeName;
+    //Encapsulation example as these are only accesed through get and set methods
+
     string employeeCompany;
     int employeeAge;
     int employeeYearsAtCompany;
 
+protected:
+    string employeeName;
+
 public:
-    // Get and Sets
+    //Get and Sets
     void setEmployeeName(string name)
     {
         employeeName = name;
@@ -41,10 +44,7 @@ public:
     {
         employeeYearsAtCompany = years;
     }
-    string getEmployeeName()
-    {
-        return employeeName;
-    }
+
     int getEmployeeAge()
     {
         return employeeAge;
@@ -57,7 +57,7 @@ public:
     {
         return employeeYearsAtCompany;
     }
-    // Employee Greeting
+    //Employee Greeting
     void greeting()
     {
         cout << "\nHello: " << employeeName << endl;
@@ -77,8 +77,12 @@ public:
             cout << employeeName << " not promoted!" << endl;
         }
     }
-
-    // Employee Constructor
+    //Polymorhpism Example
+    virtual void work()
+    {
+        cout << employeeName << " is doing work!" << endl;
+    }
+    //Employee Constructor
     Employee(string name, string company, int age, int years)
     {
         employeeName = name;
@@ -88,17 +92,73 @@ public:
     }
 };
 
+//Assigning contract Employee class to Developer
+//Inhertince example
+class Developer : public Employee
+{
+public:
+    string favoriteLanguage;
+    Developer(string name, string company, int age, int years, string language) : Employee(name, company, age, years)
+    {
+        favoriteLanguage = language;
+    }
+    // //Polymorphism
+    // void work()
+    // {
+    //     cout << employeeName << " is doing coding! in " << favoriteLanguage << endl;
+    // }
+};
+
+class Teacher : public Employee
+{
+public:
+    string classSubject;
+    Teacher(string name, string company, int age, int years, string subject) : Employee(name, company, age, years)
+    {
+        classSubject = subject;
+    }
+    void prepareLesson()
+    {
+        cout << employeeName << "Is preparing" << classSubject << "lesson" << endl;
+    }
+    //Polymorphism
+    void work()
+    {
+        cout << employeeName << " is doing work! in " << classSubject << endl;
+    }
+};
+
 int main()
 {
-    Employee employee1 = Employee("Alex", "Google", 35, 12);
-    Employee employee2 = Employee("Cynthia", "Dev.to", 15, 32);
-    // Invoke greeting method
+    Employee employee1 = Employee("Alex Macy", "Google", 35, 12);
+    Employee employee2 = Employee("Cynthia Jones", "Dev.to", 15, 32);
+    //Invoke greeting method
+
     employee1.greeting();
-    // Second Employee
-    // employee2.greeting();
-    // employee1.setEmployeeAge(2);
-    // cout << employee1.getEmployeeAge();
+    //Second Employee
+    //employee2.greeting();
+    //employee1.setEmployeeAge(2);
+    //cout << employee1.getEmployeeAge();
 
     employee1.askForPromotion();
     employee2.askForPromotion();
+
+    //Inheritance example
+    Developer developer1 = Developer("Bob Lenza", "Amazon", 42, 22, "Python");
+    developer1.askForPromotion();
+
+    //Inheritance example
+    Teacher teacher1 = Teacher("Jade Smith", "Clevland High School", 35, 6, "Math");
+    teacher1.askForPromotion();
+
+    //Polymorphism
+    // developer1.work();
+    // teacher1.work();
+
+    //Polymorphism
+    //Employee pointer holds reference to developer and teacher
+    Employee *e1 = &developer1;
+    Employee *e2 = &teacher1;
+    e1->work();
+    e2->work();
 }
